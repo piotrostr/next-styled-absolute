893c327cbc267333bc4a789365f4c53f2a10cf74
Piotr <piotr.ostrowski@aol.com>
2022-01-08 13:10:08 +0100

    add example sagas

--- src/store/counter/counter.actions.js
+++ src/store/counter/counter.actions.js
@@ -1,8 +1,10 @@
 export const types = {
   INCREMENT: "[Counter] Increment",
   INCREMENT_SUCCESS: "[Counter] Increment Success",
+  INCREMENT_FAIL: "[Counter] Increment Fail",
   DECREMENT: "[Counter] Decrement",
-  DECREMENT_SUCCESS: "[Counter] Decrement Success"
+  DECREMENT_SUCCESS: "[Counter] Decrement Success",
+  DECREMENT_FAIL: "[Counter] Increment Fail"
 }
 
 export const increment = () => ({
@@ -13,6 +15,10 @@ export const incrementSuccess = () => ({
   type: types.INCREMENT_SUCCESS
 })
 
+export const incrementFail = () => ({
+  type: types.INCREMENT_FAIL
+})
+
 export const decrement = () => ({
   type: types.DECREMENT
 })
@@ -20,3 +26,8 @@ export const decrement = () => ({
 export const decrementSuccess = () => ({
   type: types.DECREMENT_SUCCESS
 })
+
+export const decrementFail = () => ({
+  type: types.DECREMENT_FAIL
+})
+
--- src/store/counter/counter.sagas.js
+++ src/store/counter/counter.sagas.js
@@ -1,16 +1,38 @@
 import * as Actions from "store/counter/counter.actions"
 import { put, takeLatest } from "redux-saga/effects"
 
+const random = () => {
+  if (Math.floor(Math.random() * 2) == 0) {
+    return true
+  } else {
+    throw Error
+  }
+}
+
+const tryToIncrement = () => {
+  random()
+}
+
+const tryToDecrement = () => {
+  random()
+}
+
 export function* incrementSaga() {
-  console.log('asdf')
   try {
+    yield tryToIncrement()
     yield put(Actions.incrementSuccess())
   } catch (e) {
+    yield put(Actions.incrementFail())
   }
 }
 
 export function* decrementSaga() {
-  yield put(Actions.decrementSuccess())
+  try {
+    yield tryToDecrement()
+    yield put(Actions.decrementSuccess())
+  } catch (e) {
+    yield put(Actions.decrementFail())
+  }
 }
 
 export function* saga() {
